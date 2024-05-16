9a7b2b2f6d272db4f8b22af071e0628feeba48e7
yene2024 <yeneishla1524@gmail.com>
2024-05-16 16:53:44 -0400

    Fixing the code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-/* eslint-disable */
-
 const fs = require('fs');
+const fdPath = process.argv[2];
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', function (err, data) {
+fs.readFile(fdPath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
