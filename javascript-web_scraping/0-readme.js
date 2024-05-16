b4de7d32bcb2ab33c4e67c9e7eeb5760277e3e3e
yene2024 <yeneishla1524@gmail.com>
2024-05-16 13:54:35 -0400

    Writing a script that reads and prints the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* eslint-disable */
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
